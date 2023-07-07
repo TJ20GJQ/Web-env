4084d3e13cf7aaef6cc38a9d7a416e15e72f6f59
TJ20GJQ <1842280467@qq.com>
2023-07-07 17:30:40 +0800

    backpoint

--- IoT-Water/Iot-Water.py
+++ IoT-Water/Iot-Water.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import cross_origin
-from datetime import datetime
+from datetime import datetime,timedelta
 import time
 from threading import Thread
 import requests
 import json
 import random
 from flask import request
+import pandas as pd
 
 app = Flask(__name__, static_url_path='/s', static_folder='static', template_folder='templates')
 
@@ -25,11 +26,15 @@ db = SQLAlchemy(app)
 token_jxy = ""  # Token 调用IoTDA
 token_gjq = ""  # Token 调用ModelArts
 token_flag = False  # 已获取Token标识
+getData_flag = False  # 启动数据获取标识
+autoCtrl_flag = False  # 启动自动控制标识
 
 
-# 定义table WaterData
 class WaterData(db.Model):
-    __tablename__ = "Data"
+    """
+    定义存储数据表table WaterData及其column
+    """
+    __tablename__ = "waterData"
 
     id = db.Column("id", db.Integer, primary_key=True, autoincrement=True)
     datetime = db.Column(db.DateTime)
@@ -37,11 +42,17 @@ class WaterData(db.Model):
     DO = db.Column(db.Float)
     PH = db.Column(db.Float)
     TDS = db.Column(db.Float)
+    COD = db.Column(db.Float)
 
 
 def query2dict(model_list):
+    """
+    将MySQL查询到的数据对象（list/object）转为字典格式
+    :param model_list:查询结果 list/object
+    :return:dict
+    """
     if isinstance(model_list, list):  # 如果传入的参数是一个list类型的，说明是使用的all()的方式查询的
-        if isinstance(model_list[0], db.Model):  # 这种方式是获得的整个对象  相当于 select * from table
+        if isinstance(model_list[0], db.Model):  # 这种方式是获得的整个对象，相当于 select * from table
             lst = []
             for model in model_list:
                 dic = {}
@@ -49,25 +60,24 @@ def query2dict(model_list):
                     dic[col.name] = getattr(model, col.name)
                 lst.append(dic)
             return lst
-        else:  # 这种方式获得了数据库中的个别字段  相当于select id,name from table
+        else:  # 这种方式获得了数据库中的个别字段，相当于 select id,name from table
             lst = []
             for result in model_list:  # 当以这种方式返回的时候，result中会有一个keys()的属性
                 lst.append([dict(zip(result.keys, r)) for r in result])
             return lst
-    else:  # 不是list,说明是用的get() 或者 first()查询的，得到的结果是一个对象
-        if isinstance(model_list, db.Model):  # 这种方式是获得的整个对象  相当于 select * from table limit=1
+    else:  # 不是list，说明是用的get()或者first()查询的，得到的结果是一个对象
+        if isinstance(model_list, db.Model):  # 这种方式是获得的整个对象，相当于 select * from table limit=1
             dic = {}
             for col in model_list.__table__.columns:
                 dic[col.name] = getattr(model_list, col.name)
             return dic
-        else:  # 这种方式获得了数据库中的个别字段  相当于select id,name from table limit = 1
+        else:  # 这种方式获得了数据库中的个别字段，相当于 select id,name from table limit = 1
             return dict(zip(model_list.keys(), model_list))
 
 
-# 多线程实现数据实时更新
 class Async_getData:
     """
-    使用装饰器实现多线程的异步非阻塞向IoTDA的API获取数据，存入数据库
+    实现多线程的异步非阻塞向IoTDA的API获取数据，存入数据库，实现数据的实时更新
     """
     def start_async(*args):
         fun = args[0]
@@ -81,37 +91,91 @@ class Async_getData:
 
     @start_async
     def get_data_thread(*args):
+        cod_valid = pd.read_csv('COD_valid.csv', index_col=False)['fit']
+        times = 0  # 循环次数，用来存储实际污水厂COD数据
         with app.app_context():
             db.create_all()  # 在数据库中生成数据表
             db.session.commit()
-        while 1:
-            if token_flag:  # 已获取token
-                print("Getting data...")
-                url = r"https://59a6084cfa.st1.iotda-app.cn-north-4.myhuaweicloud.com:443/v5/iot/35bacf8d0d634b3f8a70fc9b5286d79d/devices/641e635340773741f9fc2714_L610_CN2023/properties?service_id=SpraySwitch"
-                Params = {"service_id": "SpraySwitch"}
-                Headers = {
-                    "X-Auth-Token": token_jxy}
-                response = requests.get(url=url, params=Params, headers=Headers)
-                res = json.loads(response.text)
+        global getData_flag
+        while token_flag and getData_flag:  # 已获取token且开启数据获取
+            # print("Getting data...")
+            url = r"https://59a6084cfa.st1.iotda-app.cn-north-4.myhuaweicloud.com:443/v5/iot/35bacf8d0d634b3f8" \
+                  r"a70fc9b5286d79d/devices/641e635340773741f9fc2714_L610_CN2023/properties?service_id=SpraySwitch"
+            Params = {
+                "service_id": "SpraySwitch"
+            }
+            Headers = {
+                "X-Auth-Token": token_jxy
+            }
+            response = requests.get(url=url, params=Params, headers=Headers)
+            res = json.loads(response.text)
+            print(res)
+            with app.app_context():
+                try:
+                    add = WaterData(datetime=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                                    temperature=res['response']['services'][0]['properties']['temperature'],
+                                    PH=res['response']['services'][0]['properties']['PH'],
+                                    DO=2.5 + 0.2*round(random.random(), 2),
+                                    TDS=0.56 + 0.1*round(random.random(), 2),
+                                    COD=cod_valid[times])
+                    db.session.add(add)
+                    db.session.commit()
+                except KeyError:  # 如果数据获取有误
+                    # add = WaterData(datetime=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), temperature=100.0,
+                    #                 PH=5.9, DO=2.5, TDS=0.56, COD=cod_valid[times])
+                    # db.session.add(add)
+                    # db.session.commit()
+                    pass
+            time.sleep(1)
+            times = times + 1
+
+
+class Async_autoCtrl:
+    """
+    实现多线程的异步非阻塞实时控制（基于对工厂实际COD数据的预测）
+    """
+    def start_async(*args):
+        fun = args[0]
+
+        def start_thread(*args, **kwargs):
+            """启动线程（内部方法）"""
+            t = Thread(target=fun, args=args, kwargs=kwargs)
+            t.start()
+
+        return start_thread
+
+    @start_async
+    def auto_ctrl_thread(*args):
+        while autoCtrl_flag:
+            with app.app_context():
+                res = query2dict(WaterData.query.all())
+                cods = []
+                for dic in res[-48:]:
+                    cods.append(dic['COD'])
+                url = "https://192.168.203.131:1027"
+                Body = {
+                    "history": cods
+                }
+                requests.packages.urllib3.disable_warnings()
+                res = requests.post(url=url, json=Body, verify=False)
+                res = json.loads(res.text)[0]['predict']
+                url = "https://192.168.203.131:1028"
+                Body = {
+                    "ctrl_data": res
+                }
+                res = requests.post(url=url, json=Body, verify=False)
+                res = json.loads(res.text)['resp_data']
                 print(res)
-                with app.app_context():
-                    try:
-                        add = WaterData(datetime=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                                        temperature=res['response']['services'][0]['properties']['MyTest'],
-                                        PH=5.9 + round(random.random(), 1), DO=2.5 + 0.2*round(random.random(), 2),
-                                        TDS=0.56 + 0.1*round(random.random(), 2))
-                        db.session.add(add)
-                        db.session.commit()
-                    except KeyError:  # 如果数据获取有误
-                        # add = WaterData(datetime=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), temperature=100.0, PH=5.9, DO=2.5, TDS=0.56)
-                        pass
                 time.sleep(1)
 
 
-# 根目录获取token，每次启动更新
 @app.route('/')
 @cross_origin()
 def start():
+    """
+    根目录，获取token，每次必须先启动以更新token
+    :return: Get Token OK / Get Token Failed
+    """
     url = r"https://iam.cn-north-4.myhuaweicloud.com/v3/auth/tokens"
     Headers = {
         "Content-Type": "application/json"
@@ -175,41 +239,113 @@ def start():
         return "Get Token Failed!"
 
 
-# 启动获取数据线程
-@app.route('/start')
+@app.route('/start_get')
 @cross_origin()
 def start_get_data():
+    """
+    启动获取数据线程
+    :return: Start get data and store into MySQL
+    """
+    global getData_flag
+    getData_flag = True
     Async_getData().get_data_thread()  # 启动异步线程
     return 'Start get data and store into MySQL.'
 
 
-# 查询设备属性数据
-@app.route('/query')
+@app.route('/stop_get')
 @cross_origin()
-def query_data():
+def stop_get_data():
+    """
+    关闭获取数据线程
+    :return: Stop get data and store into MySQL
+    """
+    global getData_flag
+    getData_flag = False
+    return 'Stop get data and store into MySQL.'
+
+
+@app.route('/query<num>')
+@cross_origin()
+def query_data(num):
+    """
+    按条数查询设备属性数据
+    :param num: 希望查询数据条数
+    :return: 查询得到的数据
+    """
     res = query2dict(WaterData.query.all())
     res_convert = []
-    for dic in res:
+    for dic in res[-int(num):]:
         dic['datetime'] = dic['datetime'].strftime('%Y-%m-%d %H:%M:%S')
         res_convert.append(dic)
-    return res_convert[-1]
+    return res_convert
+
+
+@app.route('/queryIn<period>', methods=['GET', 'POST'])
+@cross_origin()
+def query_period_data(period):
+    """
+    根据时段查询MySQL数据，使用GET方法获取最近时间段数据，使用POST方法获得历史时间段数据
+    :param period: Hour/Day/Year
+    :return: 符合时间段要求的数据
+    """
+    if request.method == 'GET':  # GET获取当前时间的区间数据
+        time_now = datetime.now()
+        # print(time_now.strftime('%Y-%m-%d %H:%M:%S'))
+        if period == 'Hour':
+            res = WaterData.query.filter(WaterData.datetime >= time_now - timedelta(hours=1)).all()
+        elif period == 'Day':
+            res = WaterData.query.filter(WaterData.datetime >= time_now - timedelta(days=1)).all()
+        elif period == 'Month':
+            res = WaterData.query.filter(WaterData.datetime >= time_now - timedelta(days=30)).all()
+        else:
+            res = []
+    else:  # POST获取历史时间的区间数据
+        time_form = request.form.to_dict()
+        if period == 'Hour':
+            time_query = datetime(int(time_form["year"]), int(time_form["month"]), int(time_form["day"]),
+                                  int(time_form["hour"]))
+            res = WaterData.query.filter(time_query <= WaterData.datetime).filter(WaterData.datetime < time_query + timedelta(hours=1)).all()
+        elif period == 'Day':
+            time_query = datetime(int(time_form["year"]), int(time_form["month"]), int(time_form["day"]))
+            res = WaterData.query.filter(time_query <= WaterData.datetime).filter(WaterData.datetime < time_query + timedelta(days=1)).all()
+        elif period == 'Month':
+            time_query = datetime(int(time_form["year"]), int(time_form["month"]), 1)
+            if int(time_form["month"]) < 12:
+                time_query_till = datetime(int(time_form["year"]), int(time_form["month"])+1, 1)
+            else:
+                time_query_till = datetime(int(time_form["year"])+1, 1, 1)
+            res = WaterData.query.filter(time_query <= WaterData.datetime).filter(WaterData.datetime < time_query_till).all()
+        else:
+            res = []
+    if len(res) >= 1:
+        res = query2dict(res)
+        res_convert = []
+        for dic in res:
+            dic['datetime'] = dic['datetime'].strftime('%Y-%m-%d %H:%M:%S')
+            res_convert.append(dic)
+        return res_convert
+    else:
+        return []
 
 
 # 使用LSTM算法预测下一个
 @app.route('/lstm')
 @cross_origin()
 def predict_data_lstm():
-    # url = "https://c8af6bb488604c9896ae462d73d7056d.apigw.cn-north-4.huaweicloud.com/v1/infers/01ee8b10-b5fa-4a7c-94bc-4f417b63a76c"
+    # url = "https://c8af6bb488604c9896ae462d73d7056d.apigw.cn-north-4.huaweicloud.com/v1/infers/01ee8b10-b5fa-4a7c-
+    # 94bc-4f417b63a76c"
     # Headers = {
     #     "X-Auth-Token": token_gjq}
-    url = "https://192.168.203.131:1026"
+    url = "https://192.168.203.131:1027"
     Body = {
-        "history": [5.479, 5.600, 5.715, 5.826, 5.933, 6.035, 6.133, 6.226, 6.315, 6.399, 6.479, 6.555, 6.626, 6.692,
-                    6.754, 6.812, 6.865, 6.913, 6.958, 6.997, 7.032, 7.063, 7.089, 7.111, 7.147, 7.145, 7.111, 7.061,
-                    6.970, 6.858, 6.829, 6.788, 6.705, 6.650, 6.578, 6.523, 6.542, 6.559, 6.551, 6.527, 6.530, 6.550,
-                    6.566, 6.595, 6.575, 6.558, 6.520, 6.480]
+        "history": [428.847, 422.564, 416.514, 410.696, 405.111, 399.758, 394.638, 389.750, 385.095, 380.672, 376.482,
+                    372.524, 368.799, 365.307, 362.046, 359.019, 356.224, 353.661, 351.331, 349.233, 347.368, 345.736,
+                    344.336, 343.168, 341.125, 340.293, 341.150, 342.997, 345.267, 347.698, 348.516, 349.393, 352.321,
+                    355.089, 356.239, 356.967, 357.737, 358.178, 354.248, 351.348, 348.909, 347.552, 353.144, 358.907,
+                    365.038, 370.885, 374.398, 377.583]
     }
     # res = requests.post(url=url, headers=Headers, json=Body)
+    requests.packages.urllib3.disable_warnings()
     res = requests.post(url=url, json=Body, verify=False)
     # print(json.loads(res.text)[0]['predict'])
     return [json.loads(res.text)[0]['predict']]
@@ -219,7 +355,8 @@ def predict_data_lstm():
 @app.route('/svm')
 @cross_origin()
 def predict_data_svm():
-    # url = "https://c8af6bb488604c9896ae462d73d7056d.apigw.cn-north-4.huaweicloud.com/v1/infers/1cbd3692-732f-4d7f-bcf7-70a0081f4606"
+    # url = "https://c8af6bb488604c9896ae462d73d7056d.apigw.cn-north-4.huaweicloud.com/v1/infers/1cbd3692-732f-4d7f-
+    # bcf7-70a0081f4606"
     # Headers = {
     #     "X-Auth-Token": token_gjq}
     url = "https://192.168.203.131:1025"
@@ -231,17 +368,23 @@ def predict_data_svm():
     res = requests.post(url=url, json=Body, verify=False)
     # print(json.loads(res.text)['data']['resp_data'][0]['predictresult'])
     return [json.loads(res.text)['data']['resp_data'][0]['predictresult']]
-    # print(request.get_json())
     
 
-# 控制第num个气泵的速度为speed
 @app.route('/ctrl_motor<num>/<speed>')
 @cross_origin()
 def control_motor(num, speed):
-    url = r"https://59a6084cfa.st1.iotda-app.cn-north-4.myhuaweicloud.com:443/v5/iot/35bacf8d0d634b3f8a70fc9b5286d79d/devices/63dcdaa2352830580e47364e_2023_3_25/commands"
+    """
+    控制第num个气泵的速度为speed
+    :param num: 气泵编号
+    :param speed: 控制电机速度 0/1/2
+    :return: 响应码（200）
+    """
+    url = r"https://59a6084cfa.st1.iotda-app.cn-north-4.myhuaweicloud.com:443/v5/iot/35bacf8d0d634b3f8a70fc9b5286d7" \
+          r"9d/devices/63dcdaa2352830580e47364e_2023_3_25/commands"
     Headers = {
         "X-Auth-Token": token_jxy,
-        'Content-Type': 'application/json'}
+        'Content-Type': 'application/json'
+    }
     Body = {
         "service_id": "Motor_Control_System",
         "command_name": f"Motor{num}_Control",
@@ -253,14 +396,22 @@ def control_motor(num, speed):
     res = command.status_code
     return str(res)
 
-# 控制第num个switch的速度为speed
+
 @app.route('/ctrl_switch<num>/<state>')
 @cross_origin()
 def control_switch(num, state):
-    url = r"https://59a6084cfa.st1.iotda-app.cn-north-4.myhuaweicloud.com:443/v5/iot/35bacf8d0d634b3f8a70fc9b5286d79d/devices/63dcdaa2352830580e47364e_2023_3_25/commands"
+    """
+    控制第num个气泵的状态为state
+    :param num: 气泵编号
+    :param state: 控制状态 ON/OFF
+    :return: 响应码（200）
+    """
+    url = r"https://59a6084cfa.st1.iotda-app.cn-north-4.myhuaweicloud.com:443/v5/iot/35bacf8d0d634b3f8a70fc9b5286d79" \
+          r"d/devices/63dcdaa2352830580e47364e_2023_3_25/commands"
     Headers = {
         "X-Auth-Token": token_jxy,
-        'Content-Type': 'application/json'}
+        'Content-Type': 'application/json'
+    }
     Body = {
         "service_id": "Switch_Control_System",
         "command_name": f"Switch{num}_Control",
@@ -272,6 +423,31 @@ def control_switch(num, state):
     command = requests.post(url=url, json=Body, headers=Headers)
     res = command.status_code
     return str(res)
+
+
+@app.route('/autoCtrl')
+@cross_origin()
+def autoCtrl():
+    """
+    启动多线程自动控制曝气
+    :return: Start running automatically
+    """
+    global autoCtrl_flag
+    autoCtrl_flag = True
+    Async_autoCtrl().auto_ctrl_thread()  # 启动异步线程
+    return 'Start running automatically.'
+
+
+@app.route('/stopAutoCtrl')
+@cross_origin()
+def stopAutoCtrl():
+    """
+    停止多线程自动控制曝气
+    :return: Stop running automatically
+    """
+    global autoCtrl_flag
+    autoCtrl_flag = False
+    return 'Stop running automatically.'
 
 
 if __name__ == '__main__':
